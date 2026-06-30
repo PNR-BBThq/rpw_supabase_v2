e2e7b1c6e12e7e36664cad740031dc9d9d05536b
PNR-BBThq <pengawasanperosak_bbt@doa.gov.my>
2026-06-30 12:00:59 +0800

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pnr-cache-v3.2"; // ⚡ Tukar versi di sini untuk paksa browser update!
+const CACHE_NAME = "pnr-cache-v3.3"; // ⚡ Tukar versi di sini untuk paksa browser update!
 const ASSETS = [
   './',
   './index.html',
