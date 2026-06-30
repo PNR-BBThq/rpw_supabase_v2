ff9021586fac6772943ac9cacc649da9ecdfe421
PNR-BBThq <pengawasanperosak_bbt@doa.gov.my>
2026-06-30 16:19:18 +0800

    Change cache name to PNR-CACHE-Version-4
    Updated cache name to force browser update.

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pnr-cache-v3.3"; // ⚡ Tukar versi di sini untuk paksa browser update!
+const CACHE_NAME = "PNR-CACHE-Version-4"; // ⚡ Tukar versi di sini untuk paksa browser update!
 const ASSETS = [
   './',
   './index.html',
