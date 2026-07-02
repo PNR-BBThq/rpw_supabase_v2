f4e957adf66fbd881ae3548269728db432797253
PNR-BBThq <pengawasanperosak_bbt@doa.gov.my>
2026-07-02 12:19:52 +0800

    Update external link to include version parameter

--- js/main.js
+++ js/main.js
@@ -95,7 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     // External Links
     const btnOpenRPW = document.getElementById('btnOpenRPW');
-    if (btnOpenRPW) btnOpenRPW.addEventListener('click', () => window.open(CONFIG.RPW_URL, '_blank'));
+    if (btnOpenRPW) btnOpenRPW.addEventListener('click', () =>window.open(CONFIG.RPW_URL + '&v=20260701', '_blank'));
     
     // Filtering (Date Inputs & Reset)
     if (typeof FilterManager !== 'undefined') {
